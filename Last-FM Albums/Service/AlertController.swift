95f97c131b5ccd87f1eed576b8b5aabba8f6179c
Nickita <Nickos_K@mail.ru>
2019-02-18 10:42:55 +0300

    Replaced textField search with searchController

---
+++ Last-FM Albums/Service/AlertController.swift
@@ -0,0 +1,21 @@
+//
+//  AlertController.swift
+//  Text Recignize
+//
+//  Created by Ringo_02 on 1/23/19.
+//  Copyright © 2019 Ringo_02. All rights reserved.
+//
+
+import UIKit
+
+class Alert  {
+    
+    static func alert(errorText: String?) -> UIAlertController {
+        
+        let alert = UIAlertController(title: "Error", message: errorText ?? "", preferredStyle: .alert)
+        let alertButton = UIAlertAction(title: "ОК", style: .default, handler: nil)
+        alert.addAction(alertButton)
+        return alert
+    }
+}
+
--- Last-FM Albums/ViewControllers/SearchViewController.swift
+++ Last-FM Albums/ViewControllers/SearchViewController.swift
@@ -14,31 +14,40 @@ class SearchViewController: UIViewController {
     private let cellReuseIdentifier = "artistCell"
     private var artists: [String]?
     private let albumsSegueIdentifier = "showAlbumsSegue"
+    private var searchText: String?
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var searchBarIsEmpty: Bool {
+        
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        
+        return searchController.isActive && !searchBarIsEmpty
+    }
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var table: UITableView!
-    @IBOutlet weak var searchArtistTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchControllerSetUp()
     }
-
-    @IBAction func refresTableData(_ sender: UIBarButtonItem) {
-        //sends a request to server and reload the table
-        activityIndicator.startAnimating()
-        guard let text = searchArtistTextField.text else { return }
-        let textForRequest = text.replacingOccurrences(of: " ", with: "+")
-        Search.forArtist(nameOfArtist: textForRequest) { names in
-            
-            self.artists = names
-            self.table.reloadData()
-            self.activityIndicator.stopAnimating()
-        }
+    //search controller declaration
+    func searchControllerSetUp() {
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Enter name of artist"
+        searchController.hidesNavigationBarDuringPresentation =
+        false
+        navigationItem.titleView = searchController.searchBar
+        definesPresentationContext = true
     }
     //hide keyboard method
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-
+        
         textField.resignFirstResponder()
         return true
     }
@@ -51,18 +60,49 @@ class SearchViewController: UIViewController {
     }
 }
 
-extension SearchViewController: UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        //updating search results after entering two characters
+        guard let searchCharNumber = searchController.searchBar.text?.count else { return }
+        if searchCharNumber > 2 {
+            
+            guard let searchBarText = searchController.searchBar.text else { return }
+            searchText = searchBarText
+            activityIndicator.startAnimating()
+            guard let text = searchText else { return }
+            let textForRequest = text.replacingOccurrences(of: " ", with: "+")
+            Search.forArtist(nameOfArtist: textForRequest) { names in
+                
+                //                    self.artists?.append("No artists found")
+                //                    self.table.reloadData()
+                //                    self.activityIndicator.stopAnimating()
+                //                    Alert.alert(errorText: "No artists found")
+                self.artists = names
+                self.activityIndicator.stopAnimating()
+                self.table.reloadData()
+            }
+        }
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //number of cells
-        return artists?.count ?? 0
+        if isFiltering {
+            
+            return artists?.count ?? 0
+        }
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as? ArtistCell else { return UITableViewCell() }
         //put our artists names into cell of table
-        cell.artistName.text = self.artists?[indexPath.row]
+        if isFiltering {
+            
+            cell.artistName.text = self.artists?[indexPath.row]
+        }
+        
         return cell
     }
 }
